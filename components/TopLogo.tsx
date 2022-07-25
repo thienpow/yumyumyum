feeb42e7cc57449883c69a03a636c8ff2eef65db
Ken Chong <thienpow@aadi.my>
2022-07-26 00:52:01 +0800

    logo component

--- components/TopLogo.tsx
+++ components/TopLogo.tsx
@@ -7,6 +7,13 @@ import { tw } from "@twind";
 export default function TopLogo() {
 
   return (
-    <a href="/" class={tw`relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none`}>YumYumYum</a>
+    <a href="/" class={tw`relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none`}>
+      <img
+          src="/logo.svg"
+          height="100px"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+      YumYumYum
+    </a>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -8,11 +8,7 @@ export default function Home() {
   return (
     <Layout>
       <div>
-        <img
-          src="/logo.svg"
-          height="100px"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
+        
         <p class={tw`my-6`}>
           Welcome to `YumYumYum`.
         </p>
