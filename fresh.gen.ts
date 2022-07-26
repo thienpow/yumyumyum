235feaa21bf8319703af654090e1298289d30caf
Ken Chong <thienpow@aadi.my>
2022-07-26 18:06:19 +0800

    generated patch

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,22 +4,20 @@
 
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/account.tsx";
-import * as $2 from "./routes/blog.tsx";
-import * as $3 from "./routes/creator.tsx";
-import * as $4 from "./routes/follower.tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/tests/erc725.tsx";
+import * as $2 from "./routes/app.tsx";
+import * as $3 from "./routes/blog.tsx";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/tests/erc725.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/account.tsx": $1,
-    "./routes/blog.tsx": $2,
-    "./routes/creator.tsx": $3,
-    "./routes/follower.tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/tests/erc725.tsx": $6,
+    "./routes/app.tsx": $2,
+    "./routes/blog.tsx": $3,
+    "./routes/index.tsx": $4,
+    "./routes/tests/erc725.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
