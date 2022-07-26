b2bff0ec298a7655a64971b86eaecae5cd03eba2
Ken Chong <thienpow@aadi.my>
2022-07-26 12:47:08 +0800

    fixing responsive blog layout/header

--- components/BlogItem.tsx
+++ components/BlogItem.tsx
@@ -8,10 +8,10 @@ export default function BlogItem(props: {datetime: string, title: string, text: 
 
   return (
     <div class={tw`mt-4 flex flex-row flex-wrap w-full`}>
-      <div class={tw`w-1/3`}>{props.datetime}</div>
-      <div class={tw`flex flex-col w-2/3`}>
-        <div class={tw`font-semibold text-2xl`}>{props.title}</div>
-        <div class={tw`py-4`}>{props.text}</div>
+      <div class={tw`py-4 w-full md:w-1/3 `}>{props.datetime}</div>
+      <div class={tw`flex flex-col w-full md:w-2/3 `}>
+        <div class={tw`font-semibold text-2xl w-full`}>{props.title}</div>
+        <div class={tw`py-4 w-full`}>{props.text}</div>
       </div>
       <hr class={tw`mt-7 h-.5 bg-black w-full`} />    
     </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header(props: { title: string }) {
 
   return (
     <div class={tw`flex flex-col justify-start w-full`}>
-      <div class={tw`text-xl font-bold text-gray-900 sm:text-5xl md:text-5xl`}>{props.title}</div>      
+      <div class={tw`font-bold text-4xl md:text-5xl text-gray-900`}>{props.title}</div>      
       <hr class={tw`mt-7 h-.5 bg-black w-full`} />
         
     </div>
